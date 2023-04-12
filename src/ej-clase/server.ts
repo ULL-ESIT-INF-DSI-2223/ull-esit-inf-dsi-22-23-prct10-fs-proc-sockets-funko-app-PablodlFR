a4d771a28ca86be6532d3b359a3b90fb9abbd7ea
PablodlFR <alu0101336152@ull.edu.es>
2023-04-12 10:02:50 +0000

    Ej-clase

--- src/ej-clase/server.ts
+++ src/ej-clase/server.ts
@@ -15,7 +15,6 @@ import {spawn} from 'child_process';
         const command = spawn(message.command, [...message.argv]);
 
         command.stdout.on('data', (output) => {
-          // enviar el resultado al cliente
           connection.write(JSON.stringify({'type': 'command', 'output': output.toString()}) + '\n');
         });
 
@@ -25,21 +24,9 @@ import {spawn} from 'child_process';
       }
     });
 
-    // connection.on('error', (err) => {
-    //   console.error("Error", err);
-    // })
-
-    // connection.on('end', () => {
-    //   const message = JSON.parse(wholeData);
-    //   console.log(message.test);
-    // })
-
     connection.on('close', () => {
       console.log('Un cliente se ha desconectado.');
     });
   }).listen(60300, () => {
     console.log('Esperando a que un cliente se conecte.');
   });
-//}
-
-
